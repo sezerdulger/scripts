62d28453c671aa42eb4ad04f74b276f7b49cf578
Sezer <sezerdulger@gmail.com>
2019-04-14 14:26:34 +0300

    executable

--- kubeadm-single.sh
+++ kubeadm-single.sh
@@ -1,3 +1,4 @@
+#!/bin/bash -ex
 apt-get update
 apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
